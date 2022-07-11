7d5aa22a9f33cf61c8f86a69af3994acff1f1a2b
Sanjay <sanjaymahabir9@gmail.com>
2022-07-11 10:22:47 -0400

    Changes to landing/home page

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./Server/Config/app"));
 const debug_1 = __importDefault(require("debug"));
-(0, debug_1.default)('lesson3:server');
+(0, debug_1.default)('pollaliens:server');
 const http_1 = __importDefault(require("http"));
 const port = normalizePort(process.env.PORT || '3000');
 app_1.default.set('port', port);
--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@
 
 import app from './Server/Config/app';
 import debug from 'debug';
-debug('lesson3:server');
+debug('pollaliens:server');
 import http from 'http';
 import { HttpError } from 'http-errors';
 
